aae72d4aefd67d575469fc7872692569e7257ceb
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-04-09 22:12:15 +0530

    home page created

---
+++ src/components/Home.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const Home = () => {
+    return (
+        <div>
+
+            <NavBar />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://english.mathrubhumi.com/image/contentid/policy:1.7662534:1656931322/New%20Project.jpg?$p=8ce1a8e&f=16x10&w=852&q=0.8" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://s1.rdbuz.com/bo-images/IND/WM/27000/1934/FR/ML/webp/VztESv.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://images.livemint.com/img/2023/02/22/600x338/bengaluru_1677042673712_1677042680029_1677042680029.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://www.volvobuses.com/content/dam/volvo-buses/markets/global/classic/news/2020/1860x1050-Volvo-Bus-KSRTC-2020.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://www.india.com/wp-content/uploads/2023/08/KSRTC.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://www.team-bhp.com/sites/default/files/styles/check_high_res/public/kerala-srtc.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://media.assettype.com/thenewsminute%2Fimport%2Fsites%2Fdefault%2Ffiles%2Fksrtc_new_0.jpg?w=480&auto=format%2Ccompress&fit=max" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://s1.rdbuz.com/bo-images/IND/WM/19393/1118/FR/ML/webp/0M8G8n.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
