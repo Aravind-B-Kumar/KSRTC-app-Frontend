d23ef31f37f8c2955d1f95daacb950990bb50cfc
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-04-09 23:04:33 +0530

    Route added and minor changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Add from './components/Add';
 import Search from './components/Search';
 import Delete from './components/Delete';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
 
       <BrowserRouter>
-        
+        <Routes>
+          
+          <Route path='/' element={<Home/>}/>
+          <Route path='/search' element={<Search/>}/>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/delete' element={<Delete/>}/>
+
+        </Routes>
       </BrowserRouter>
 
-    </div>
+
   );
 }
 
--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -40,7 +40,7 @@ const Add = () => {
                             </div>
 
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button className="btn btn-success">Submit</button>
+                                <button className="btn btn-success">Add</button>
                             </div>
 
                         </div>
