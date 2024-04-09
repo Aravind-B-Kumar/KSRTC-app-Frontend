40deba90ee173f759e13104565048a6e0896c7d3
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-04-09 22:55:27 +0530

    Delete page created

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Add from './components/Add';
 import Search from './components/Search';
+import Delete from './components/Delete';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div>
 
-      <Search/>
+      <BrowserRouter>
+        
+      </BrowserRouter>
 
     </div>
   );
